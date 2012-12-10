e3c3ac081c645cb22a35f5197f98c43dd257db58
stebon24 <stebon24@gmail.com>
2012-12-09 18:23:50 -0800

    .all() method added

--- requester.js
+++ requester.js
@@ -7,7 +7,8 @@
 var request = require('request')
 ,	EventEmitter = require('events').EventEmitter
 ,	util = require('util')
-,	qs = require('querystring');
+,	qs = require('querystring')
+,	url = require('url');
 
 /*==========  UTILITY FUNCTIONS  ==========*/
 
@@ -19,33 +20,108 @@ var parseJSON = function(myJSON, cb){
 /*==========  CONSTRUCTOR  ==========*/
 
 var Requester = function(path) {
-		this.path = path;
-		this.filter = '';
-		this.query = '';
 		this.ee = new EventEmitter();
+		this.filter = '';
+		this.url = {
+			protocol: 'http'
+		,	host: 'www.reddit.com'
+		,	pathname : path + this.filter + '/.json'
+		,	query: {}
+		};
 	};
 
-/*==========  QUERY OPTIONS  ==========*/
 
-//adds a sort query to the request
-Requester.prototype.from = function(sortOption,cb){
-	this.query += (this.query) ? '&t=' + sortOption : '?t=' +  sortOption;
+/*========== @QUERY OPTIONS  ==========*/
+
+//adds a sort filter for USER QUERY ONLY, r QUERIES should use chaining filters to sort
+Requester.prototype.sortBy = function(option, cb){
+	this.url.query.sort = option;
 	return (cb) ? this.exe(cb) : this;
 };
 
-//adds a limit query to the request
+//adds a time filter to query
+Requester.prototype.from = function(time, cb){
+	this.url.query.t = time;
+	return (cb) ? this.exe(cb) : this;
+};
+
+//adds a limit filter to query
 Requester.prototype.limit = function(limit, cb){
-	this.query += (this.query) ? '&limit=' + limit  : '?limit=' +  limit;
+	this.url.query.limit = limit;
 	return (cb) ? this.exe(cb) : this;
 };
 
-/*==========  REQUEST EXECUTION METHODS  ==========*/
+//adds an after filter to query
+Requester.prototype.after = function(after, cb){
+	this.url.query.after = after;
+	return (cb) ? this.exe(cb) : this;
+};
+
+//adds a before filter to query
+Requester.prototype.before = function(before, cb){
+	this.url.query.before = before;
+	return (cb) ? this.exe(cb) : this;
+};
+
+//adds a count filter to query
+Requester.prototype.count = function(count, cb){
+	this.url.query.count = count;
+	return (cb) ? this.exe(cb) : this;
+};
+
+/*========== @REQUEST EXECUTION METHODS  ==========*/
 
 //executes a single request
 Requester.prototype.exe = function(cb) {
-	var url = this.path + '/' + this.filter + '/' + '.json' + this.query;
-		request(url, cb);
-		console.log('Requested URL: ' + url);
+	var query = qs.stringify(this.url.query)
+	,	reqUrl = url.format(this.url);
+
+		request(reqUrl, cb);
+		console.log('Requested URL: ' + reqUrl);
+};
+
+//executes multiple requests
+Requester.prototype.all = function(cb) {
+		cb(this.ee);
+		this.url.query.limit = 100; //force max limit for bulk requests
+		this.collector();
+};
+
+/*
+	collector calls itself recursivly to make multiple requests when
+	it becomes actiavted by the .all() method. It emits a data event
+	each time a request completes and passes the response data to the event
+	listener.
+
+ */
+
+Requester.prototype.collector = function() {
+	var that = this;
+	var reqUrl = url.format(that.url);
+	
+	console.log('Requested: ' + reqUrl);
+
+	request(reqUrl, function(error, res, body){
+		if (error) {
+			return that.ee.emit('error', error);
+		}
+		if (!error && res.statusCode == 200) {
+			that.ee.emit('data', body);
+
+			parseJSON(body, function(bodyObj){
+				
+				if(bodyObj.data.after) {
+					var nextAfter = bodyObj.data.after,
+						prevAfter = that.url.query.after;
+						that.url.query.count += 100;
+						that.url.query.after = nextAfter;
+
+						return (nextAfter !== prevAfter) ?
+							that.collector() : that.ee.emit('end', 'Done');
+				}
+			});
+		}
+	});
 };
 
 /*==========  CHAINING FILTERS  ==========*/
