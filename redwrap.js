6261ef972df652107cfc8adfbe5b3ed1c35ebf50
ShaneK <shane@shanessite.net>
2014-03-19 19:07:24 -0500

    Made a setUserAgent function that allows users to set the user agent of redwrap

--- redwrap.js
+++ redwrap.js
@@ -1,35 +1,43 @@
 Requester = require('./requester').Requester;
 
+var userAgent = "redwrap";
+
+var setUserAgent = function(name){
+    userAgent = name;
+};
+
 //request user data
 var user = function(username, cb) {
-		var path = '/user/' + username + '/',
-			requester = new Requester(path);
+    var path = '/user/' + username + '/',
+        requester = new Requester(path, userAgent);
 
-		return(cb) ? requester.exe(cb) : requester;
-	};
+    return(cb) ? requester.exe(cb) : requester;
+};
 
 //request subreddit data
 var r = function(subreddit, cb) {
-		var path = '/r/' + subreddit + '/',
-			requester = new Requester(path);
+    var path = '/r/' + subreddit + '/',
+        requester = new Requester(path, userAgent);
 
-		return(cb) ? requester.exe(cb) : requester;
-	};
+    return(cb) ? requester.exe(cb) : requester;
+};
 
 //lists reddit front page filters
 var list = function(filter, cb) {
-		var path = '',
-			requester = '';
+    var path = '',
+        requester = '';
 
-		if(filter) {
-			path = filter + '/', requester = new Requester(path);
-		} else {
-			requester = new Requester();
-		}
+    if(filter) {
+        path = filter + '/';
+        requester = new Requester(path, userAgent);
+    } else {
+        requester = new Requester(null, userAgent);
+    }
 
-		return(cb) ? requester.exe(cb) : requester;
-	};
+    return(cb) ? requester.exe(cb) : requester;
+};
 
+exports.setUserAgent = setUserAgent;
 exports.user = user;
 exports.r = r;
 exports.list = list;
